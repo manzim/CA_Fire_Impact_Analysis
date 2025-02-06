fe83a3e339749ad3dcba42988918f493e1688e21
manzim <manzim.ruet@gmail.com>
2025-02-07 00:25:53 +0100

    added NOAA dataset, removed unnecessary files, refactored code

--- scripts/dataset_loader_function.py
+++ scripts/dataset_loader_function.py
@@ -1,7 +1,14 @@
 import pandas as pd
 
+# original dataset
+# downloaded from: https://data.ca.gov/dataset/cal-fire-damage-inspection-dins-data
 def load_dataset():
-    return pd.read_csv('../dataset/POSTFIRE_MASTER_DATA_SHARE_2064760709534146017.csv')  # Correct
+    return pd.read_csv('../dataset/POSTFIRE_MASTER_DATA_SHARE_2064760709534146017.csv') 
 
+# Cleaned dataset
 def load_cleaned_dataset():
     return pd.read_csv('../dataset/dataset_cleaned.csv')
+
+# NOAA Weather Data dataset
+def load_weather_dataset():
+    return pd.read_csv('../dataset/Weather_data.csv') 
